e4ba185fd8cf54f76d78d91647f94a890f5b74ce
Brandon Lockaby <gbrandon@gmail.com>
2012-01-03 21:34:51 -0500

    fix copyright notice

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-/*
-	
-	This file is part of OhHi
-	http://github.com/brandon-lockaby/OhHi
-	
-	(c) Brandon Lockaby http://about.me/brandonlockaby for http://oh-hi.info
-	
-	OhHi is free software licensed under Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
-	http://creativecommons.org/licenses/by-nc-sa/3.0/
-	
-*/
-
 <?php
+	/*
+		
+		This file is part of OhHi
+		http://github.com/brandon-lockaby/OhHi
+		
+		(c) Brandon Lockaby http://about.me/brandonlockaby for http://oh-hi.info
+		
+		OhHi is free software licensed under Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
+		http://creativecommons.org/licenses/by-nc-sa/3.0/
+		
+	*/
+	
 	$cwd = str_replace('\\', '/', getcwd());
 	$swd = dirname($_SERVER["SCRIPT_NAME"]);
 	if(substr($cwd, -strlen($swd)) === $swd) {
